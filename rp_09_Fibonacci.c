19d0fab27e5695a5b0ea7873d344e7b4f81d474d
conchboy <panchaocn@hotmail.com>
2021-02-17 01:09:09 +0800

    斐波那契数列

---
+++ rp_09_Fibonacci.c
@@ -0,0 +1,30 @@
+//
+// Created by C2243829 on 2021/2/17.
+//
+#include "stdio.h"
+int main(){
+    int n;
+    int i;
+    long int f3, f2, f1;
+    f2 = 2;
+    f1 = 1;
+    printf("请输入你需要求的项的序列:");
+    scanf("%d", &n);
+    if (1 == n){
+        f3 = 1;
+    }
+    else if (2 == n){
+        f3 = 2;
+    }
+    else{
+
+        for (i = 3;  i <= n; ++i) {
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3;
+        }
+    }
+    printf("n = %d", f3);
+
+    return 0;
+}
