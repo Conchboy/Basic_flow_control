27c46aebecfc53dff6dee6459c09e3ce35066f7b
conchboy <panchaocn@hotmail.com>
2021-02-17 00:14:40 +0800

    循环嵌套的试题07

---
+++ rp_07_nesting_for.c
@@ -0,0 +1,16 @@
+//
+// Created by C2243829 on 2021/2/17.
+//
+#include <stdio.h>
+int main(){
+    int i, j;
+    for (int i = 0; i < 3; ++i) {
+        printf("ºÙºÙ!\n");
+        for (int j = 2; j < 5; ++j) {
+            printf("¹ş¹ş!\n");
+        }
+    }
+printf("ÎûÎû!\n");
+
+    return 0;
+}
